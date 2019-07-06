dcf9ac05bae64265652102049a660ad54b62e140
Marcus Callender <m.shute@outlook.com>
2019-07-06 22:11:35 +0100

    Initial Commit

---
+++ Assets/Movment.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody m_rigb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_rigb.AddForce(new Vector3(0.0f, 100.0f, 0.0f));
+        }
+
+        Vector3 movment = new Vector3(Input.GetAxisRaw("Horizontal"), m_rigb.velocity.y, Input.GetAxisRaw("Vertical"));
+        movment *= 8.0f;
+
+        m_rigb.AddForce(movment);*/
+
+        Vector3 movment = new Vector3(Input.GetAxisRaw("Horizontal"), m_rigb.velocity.y, Input.GetAxisRaw("Vertical"));
+        movment *= 8.0f;
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            movment.y = 10.0f;
+        }
+        else if (!IsGrounded())
+        {
+            movment.y -= 9.81f * Time.deltaTime;
+        }
+
+        m_rigb.velocity = movment;
+
+        Debug.Log("movment: " + movment);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, 1.1f);
+    }
+}
---
+++ Assets/SetRenderInstanceShaderVar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRenderInstanceShaderVar : MonoBehaviour
+{
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    [SerializeField]
+    private int var = 1;
+
+    [SerializeField]
+    private Color Color;
+
+    void Awake()
+    {
+        _propBlock = new MaterialPropertyBlock();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _renderer.GetPropertyBlock(_propBlock);
+
+        Debug.Log(gameObject.name + ", RefNumber: " + _propBlock.GetInt("_RefNumber"));
+
+        _propBlock.SetInt("_RefNumber", var);
+        _propBlock.SetColor("_Color", Color);
+        _renderer.SetPropertyBlock(_propBlock);
+    }
+}
