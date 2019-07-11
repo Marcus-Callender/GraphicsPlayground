03200f7181003effd221e970ecda771b0080fee2
Marcus Callender <m.shute@outlook.com>
2019-07-11 11:39:40 +0100

    Hilighted objects now pulse

--- Assets/GameplayScripts/OutlineHilighter.cs
+++ Assets/GameplayScripts/OutlineHilighter.cs
@@ -4,38 +4,36 @@ using UnityEngine;
 
 public class OutlineHilighter : MonoBehaviour
 {
-    [SerializeField]
-    private Color m_unhilightedColor;
-
-    [SerializeField]
-    private Color m_hilightedColor;
+    private OutlineVariables m_variables;
 
     private Color m_colorToUse;
 
-    private Renderer _renderer;
-    private MaterialPropertyBlock _propBlock;
+    private Renderer m_renderer;
+    private MaterialPropertyBlock m_propBlock;
 
     void Awake()
     {
-        _propBlock = new MaterialPropertyBlock();
-        _renderer = GetComponent<Renderer>();
+        m_propBlock = new MaterialPropertyBlock();
+        m_renderer = GetComponent<Renderer>();
+
+        m_variables = OutlineVariables.GetInstance();
     }
 
     // OutlineHilighter has been moved up in the script exicution order so this will run first
     void Update()
     {
-        m_colorToUse = m_unhilightedColor;
+        m_colorToUse = m_variables.m_unhilightedColor;
     }
 
     public void Hilight()
     {
-        m_colorToUse = m_hilightedColor;
+        m_colorToUse = Color.Lerp(m_variables.m_hilightedColor1, m_variables.m_hilightedColor2, (Mathf.Sin(Time.time * m_variables.m_hilightPulseSpeed) + 1) / 2f);
     }
 
     private void LateUpdate()
     {
-        _renderer.GetPropertyBlock(_propBlock);
-        _propBlock.SetColor("_OutlineColor", m_colorToUse);
-        _renderer.SetPropertyBlock(_propBlock);
+        m_renderer.GetPropertyBlock(m_propBlock);
+        m_propBlock.SetColor("_OutlineColor", m_colorToUse);
+        m_renderer.SetPropertyBlock(m_propBlock);
     }
 }
---
+++ Assets/GameplayScripts/OutlineVariables.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// static serialized variables can't be edited in editor
+public class OutlineVariables : MonoBehaviour
+{
+    private static OutlineVariables m_instance;
+
+    public static OutlineVariables GetInstance()
+    {
+        return m_instance;
+    }
+    
+    [SerializeField]
+    public Color m_unhilightedColor;
+
+    [SerializeField]
+    public Color m_hilightedColor1;
+
+    [SerializeField]
+    public Color m_hilightedColor2;
+
+    [SerializeField]
+    public float m_hilightPulseSpeed = 2.0f;
+    
+    void Awake()
+    {
+        if (m_instance == null)
+            m_instance = this;
+        else
+        {
+            Debug.LogError("OutlineVariables instance already exists.");
+            Destroy(gameObject);
+        }
+    }
+}
